dc865ed388bbdc52a5262e444594656a57b70b53
marciobarruda <marciobarruda@recife.pe.gov.br>
2026-04-29 15:22:20 -0300

    Bump version to 51 (v1.17) to fix update error 500

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.marcioarruda.clubedodomino"
         minSdk = 26
         targetSdk = 34
-        versionCode = 50
-        versionName = "1.15"
+        versionCode = 51
+        versionName = "1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
